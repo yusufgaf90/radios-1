daa98ab07c1991eaeebb9bbe45975f095ef037af
Yusuf Gafari <43988286+yusufgaf@users.noreply.github.com>
2022-01-12 17:54:33 +0000

    id changed
    id changed

--- services/Urhitsradio.js
+++ services/Urhitsradio.js
@@ -6,7 +6,7 @@ module.exports = () => {
     "name": "Urhitsradio",
     "aliases": ["Urhitsradio", "Uh", "UHR"],
     "logo": "https://uhrlive.uk/images/logo.png",
-    "id": "f3540611-0f7c-4f99-a3ec-e6775d8d1991",
+    "id": "3f6cbcb4-a6fd-4449-a766-565e0f60f655",
     "tagline": "Urhitsradio is an online licence Internet radio station that plays a huge variety of the freshest & best hits from across the many genres of music which are presented by our Radio DJ's in the UK & Worldwide. Our Fun and Energetic DJ's Are Here to Make Some Noise and Turn on the Beat.",
     "tags": [tags.pop, tags.community],
     "language": langs.where('1', 'en'),
